1123b47c9e8626b36c5ab7d094b694d64f8358ee
MintTree117 <135687103+MintTree117@users.noreply.github.com>
2024-07-10 18:30:37 -0400

    Updated order placed email.

--- OrderingApplication/Features/Ordering/Services/OrderingEmailUtility.cs
+++ OrderingApplication/Features/Ordering/Services/OrderingEmailUtility.cs
@@ -5,105 +5,100 @@ namespace OrderingApplication.Features.Ordering.Services;
 
 internal static class OrderingEmailUtility
 {
-    internal static string GenerateOrderPlacedEmail(Order order)
+    internal static string GenerateOrderPlacedEmail( Order order )
     {
         var emailHtml = new StringBuilder();
 
-        const string top = """
-                           <html>
-                           <head>
-                           <style>
-                           body { font-family: Arial, sans-serif; }
-                           .order-details { width: 100%; border-collapse: collapse; margin: 20px 0; }
-                           .order-details th, .order-details td { border: 1px solid #dddddd; text-align: left; padding: 8px; }
-                           .order-details th { background-color: #f2f2f2; }
-                           .order-header { font-size: 18px; font-weight: bold; margin-bottom: 10px; }
-                           .order-section { margin-bottom: 20px; }
-                           </style>
-                           </head>
-                           """;
+        const string top =
+            """
+            <html>
+                <head>
+                    <style>
+                        body { font-family: Arial, sans-serif; }
+                        .order-details { width: 100%; border-collapse: collapse; margin: 20px 0; }
+                        .order-details th, .order-details td { border: 1px solid #dddddd; text-align: left; padding: 8px; }
+                        .order-details th { background-color: #f2f2f2; }
+                        .order-header { font-size: 18px; font-weight: bold; margin-bottom: 10px; }
+                        .order-section { margin-bottom: 20px; }
+                    </style>
+                </head>
+            """;
 
         emailHtml.AppendLine( top );
-        emailHtml.AppendLine($"""
-        <body>
-        <div class='order-header'>Order Confirmation</div>
-        <div class='order-section'>Order ID: {order.Id}</div>
-        <div class='order-section'>Date Placed: {order.DatePlaced}</div>
-        <div class='order-section'>Total Quantity: {order.TotalQuantity}</div>
-        <div class='order-section'>Order Status: {order.State}</div>
-        <div class='order-header'>Contact Information</div>
-        <div class='order-section'>Name: {order.CustomerName}</div>
-        <div class='order-section'>Email: {order.CustomerEmail}</div>
-        <div class='order-section'>Phone: {order.CustomerPhone}</div>
-        <div class='order-header'>Shipping Address</div>
-        <div class='order-section'>{order.OrderAddress.BillingAddressName}</div>
-        <div class='order-section'>{order.OrderAddress.BillingPosX}</div>
-        <div class='order-section'>{order.OrderAddress.BillingPosY}</div>
-        <div class='order-header'>Billing Address</div>
-        <div class='order-section'>{order.OrderAddress.ShippingAddressName}</div>
-        <div class='order-section'>{order.OrderAddress.ShippingPosX}</div>
-        <div class='order-section'>{order.OrderAddress.ShippingPosY}</div>
-        <div class='order-header'>Order Details</div>
-        <table class='order-details'>
-        <tr>
-        <th>Group ID</th>
-        <th>Warehouse ID</th>
-        <th>State</th>
-        <th>Last Updated</th>
-        </tr>
-        """);
+        emailHtml.AppendLine(
+            $"""
+             <body>
+                 <div class='order-header'>Order Confirmation</div>
+                 <div class='order-section'>Order ID: {order.Id}</div>
+                 <div class='order-section'>Date Placed: {order.DatePlaced}</div>
+                 <div class='order-section'>SubTotal: {order.SubTotal}</div>
+                 <div class='order-section'>SubTotal: {order.ShippingCost}</div>
+                 <div class='order-section'>Shipping Cost: {order.TotalDiscount}</div>
+                 <div class='order-section'>Total Discount: {order.TaxAmount}</div>
+                 <div class='order-section'>Total Price: {order.TotalPrice}</div>
+                 <div class='order-section'>Total Quantity: {order.TotalQuantity}</div>
+                 <div class='order-section'>Order Status: {order.State}</div>
+                 <div class='order-header'>Order Status</div>
+                 <div class='order-section'>Status: {order.State}</div>
+                 <div class='order-header'>Contact Information</div>
+                 <div class='order-section'>Name: {order.CustomerName}</div>
+                 <div class='order-section'>Email: {order.CustomerEmail}</div>
+                 <div class='order-section'>Phone: {order.CustomerPhone}</div>
+                 <div class='order-header'>Shipping Address</div>
+                 <div class='order-section'>{order.OrderAddress.BillingAddressName}</div>
+                 <div class='order-section'>{order.OrderAddress.BillingPosX}</div>
+                 <div class='order-section'>{order.OrderAddress.BillingPosY}</div>
+                 <div class='order-header'>Billing Address</div>
+                 <div class='order-section'>{order.OrderAddress.ShippingAddressName}</div>
+                 <div class='order-section'>{order.OrderAddress.ShippingPosX}</div>
+                 <div class='order-section'>{order.OrderAddress.ShippingPosY}</div>
+                 <div class='order-header'>Order Details</div>
+             """ );
 
         foreach (var group in order.OrderGroups)
         {
-            emailHtml.AppendLine($"""
-            <tr>
-            <td>{group.Id}</td>
-            <td>{group.WarehouseId}</td>
-            <td>{group.State}</td>
-            <td>{group.LastUpdated:MMMM dd, yyyy}</td>
-            </tr>
-            <tr>
-            <td colspan='4'>
-            <table class='order-details' style='margin-left: 20px;'>
-            <tr>
-            <th>Order Line ID</th>
-            <th>Unit ID</th>
-            <th>Unit Price</th>
-            <th>Quantity</th>
-            <th>Discount</th>
-            <th>Tax</th>
-            </tr>
-            """);
+            emailHtml.AppendLine(
+                $"""
+                 <div class='order-header'>Shipping Group</div>
+                 <div class='order-section'>Id: {group.Id}</div>
+                 <div class='order-section'>Id: {group.State}</div>
+                 """);
+            
 
-            foreach (var line in group.OrderLines)
+            foreach ( var line in group.OrderLines )
             {
-                emailHtml.AppendLine($"""
-                <tr>
-                <td>{line.Id}</td>
-                <td>{line.UnitId}</td>
-                <td>{line.UnitPrice:C}</td>
-                <td>{line.Quantity}</td>
-                <td>{line.UnitDiscount:C}</td>
-                </tr>
-                """);
+                emailHtml.AppendLine(
+                    $"""
+                     <table class='order-details'>
+                         <tr>
+                             <th>Unit ID</th>
+                             <th>Unit Price</th>
+                             <th>Quantity</th>
+                             <th>Discount</th>
+                             <th>Tax</th>
+                         </tr>
+                     <tr>
+                     <tr>
+                         <td>{line.Id}</td>
+                         <td>{line.UnitId}</td>
+                         <td>{line.UnitPrice:C}</td>
+                         <td>{line.UnitDiscount:C}</td>
+                         <td>{line.ShippingCost:C}</td>
+                         <td>{line.Quantity}</td>
+                     </tr>
+                     """ );
             }
-
-            emailHtml.AppendLine("""
-            </table>
-            </td>
-            </tr>
-            """);
         }
 
-        emailHtml.AppendLine("""
-        </table>
-        </body>
-        </html>
-        """);
+        emailHtml.AppendLine(
+            """
+                    </table>
+                </body>
+            </html>
+            """ );
 
         return emailHtml.ToString();
     }
-
     internal static string GenerateOrderGroupUpdateEmail( OrderGroup group, OrderState newState )
     {
         var emailHtml = new StringBuilder();
@@ -159,7 +154,6 @@ internal static class OrderingEmailUtility
 
         return emailHtml.ToString();
     }
-
     internal static string GenerateOrderUpdateEmail( Order order, OrderState newState )
     {
         return @"
